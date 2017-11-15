5c00b6724f52adaa4cf46d27415df9884ab8dfbe
deltmd <remidy@gmail.com>
2017-11-15 16:03:51 -0700

    Utilized the readme to build the redux for the wizards instead of passing params into the router.js.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+import { HashRouter } from 'react-router-dom';
+import { Provider }  from 'react-redux';
+import store from './store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+  <HashRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </HashRouter>
+  , document.getElementById('root'));
 registerServiceWorker();
 
